55f780af00ed0260e22452c3c5ceb024fdf588f7
ramin <ramin@RAMIN-PC>
2021-03-01 02:05:23 +0330

    ..

--- API/Program.cs
+++ API/Program.cs
@@ -22,6 +22,7 @@ namespace API
             {
                 var context = services.GetRequiredService<DataContext>();
                 context.Database.Migrate();
+                
             }
             catch (Exception ex)
             {
