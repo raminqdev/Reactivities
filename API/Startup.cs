4b5d2af0c8061c0e095cbea031c954c3ac939f83
ramin <ramin@RAMIN-PC>
2021-03-01 16:46:58 +0330

    ..

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,7 +37,7 @@ namespace API
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:300");
-                })
+                });
             });
         }
 
