d8bf477a747fcd9cd0af5969d4154876be1aacaf
ramin <ramin@RAMIN-PC>
2021-03-02 17:11:26 +0330

    Commad and Query ...

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _dataContext.Add(request.Activity);
+
+                await _dataContext.SaveChangesAsync();
+                
+                return Unit.Value;
+            }
+        }
+    }
+
+}
