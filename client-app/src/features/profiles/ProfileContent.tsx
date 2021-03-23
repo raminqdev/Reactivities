80ee8814a87e49d32256030f688785dd49010526
raminqdev <ramin.qamari93@gmail.com>
2021-03-23 18:59:35 +0430

    client app, Image upload...

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -4,7 +4,7 @@ import { Tab } from 'semantic-ui-react';
 export default function ProfileContent() {
     const panes = [
         { menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane> },
-        { menuItem: 'Photos', render: () => <Tab.Pane>Photos Content</Tab.Pane> },
+        { menuItem: 'Photos', render: () => <Tab.Pane>About Content</Tab.Pane> },
         { menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane> },
         { menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane> },
         { menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane> },
