74a233d49801794551f19bf655b86f3d8f9faa48
raminqdev <ramin.qamari93@gmail.com>
2021-03-27 19:47:25 +0430

    User Following

---
+++ API/Controllers/FollowController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class FollowController : BaseApiController
+    {
+        [HttpPost("{username}")]
+        public async Task<IActionResult> CreateActivity(string username)
+        {
+            return HandleResult(await Mediator.Send(new FollowToggle.Command {TargetUsername = username}));
+        }
+    }
+}
