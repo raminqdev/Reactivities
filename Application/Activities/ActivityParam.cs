80459e2842041d8e7bbaf598157bc6e1969a7be5
raminqdev <ramin.qamari93@gmail.com>
2021-04-01 19:38:41 +0430

    Filtering

---
+++ Application/Activities/ActivityParam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Application.Core;
+
+namespace Application.Activities
+{
+    public class ActivityParam : PagingParams
+    {
+        public bool IsGoing { get; set; }
+        public bool IsHost { get; set; }
+
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,20 +1,37 @@
+import { observer } from 'mobx-react-lite'
 import React from 'react'
 import Calendar from 'react-calendar'
 import { Header, Menu } from 'semantic-ui-react'
+import { useStore } from '../../../app/stores/store'
 
-function ActivityFilters() {
+export default observer(function ActivityFilters() {
+  const { activityStore: { predicate, setPredicate } } = useStore();
+
   return (
     <>
       <Menu vertical size='large' style={{ width: '100%', marginTop: 26 }}>
         <Header icon='filter' attached color='teal' content='Filters' />
-        <Menu.Item content='All activities' />
-        <Menu.Item content="I'm Going" />
-        <Menu.Item content="I'm hosting " />
+        <Menu.Item
+          content='All activities'
+          active={predicate.has('all')}
+          onClick={() => setPredicate('all', 'true')}
+        />
+        <Menu.Item
+          content="I'm Going"
+          active={predicate.has('isGoing')}
+          onClick={() => setPredicate('isGoing', 'true')}
+        />
+        <Menu.Item
+          content="I'm hosting "
+          active={predicate.has('isHost')}
+          onClick={() => setPredicate('isHost', 'true')}
+        />
       </Menu>
       <Header />
-      <Calendar />
+      <Calendar
+        onChange={(date) => setPredicate('startDate', date as Date)}
+        value={predicate.get('startDate') || new Date()}
+      />
     </>
   )
-}
-
-export default ActivityFilters
+})
